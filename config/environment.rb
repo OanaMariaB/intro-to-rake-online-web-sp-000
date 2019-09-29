d583d9f46646d90042bc04c27b92036ba719c79e
Oana Berentan <oana.berentan@yahoo.com>
2019-09-29 15:41:39 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,3 @@ require 'pry'
 require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
-
-namespace :db do
-desc 'migrate changes to your database'
-task :migrate => :environment do
-  Student.create_table
-end
-end
